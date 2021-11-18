3859f613e0b2986eee506ace29d0177d74289db4
Ryosuke-Shigi <adalovelace6365@gmail.com>
2021-11-18 17:37:39 +0900

    追加

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/tickets','api\ticketsController@sales_interval');
 
+
+//sales_intervalのstartとendの間にあるチケットのネームとコード（重複する文分ははぶく）を返す
+Route::get('/tickets_code_name','api\ticketsController@sales_interval_ticket_code_name');
+
+////sales_intervalのstartとendの間にあるチケットをさらにチケットコードで抽出し、そのデータのチケット名、チケット紹介内容、注意事項を返す
 Route::get('/tickets_ticket_code','api\ticketsController@sales_interval_ticket_code');
+
+
+//sales_intervalのstartとendの間にあるチケットの[ticket_name contents_data cautions_text]を取得する（チケットコードでも抽出)
+Route::get('/tickets_ticket_code_detail','api\ticketsController@sales_interval_ticket_code_detail');
 
 //post チケット登録
 Route::post('/tickets/reserve','api\ticketsController@ticket_reserve');
